8c05a9741ace09bc0376d774377333d3f5bb3b87
AstroeHxro <memrieit@gmail.com>
2025-07-29 14:42:14 -0400

    finished typing out answer keys

--- quiz.py
+++ quiz.py
@@ -1,95 +1,136 @@
-keys = {
-    'Reed Richards': 'Mr. Fantastic',
-    'Peter Parker': 'Spider-man',
-    'Bruce Banner': 'The Hulk',
-}
-
-names = ['Mr. Fantastic', 'Spider-Man', 'The Hulk']
-answers = ['Reed Richards', 'Peter Parker', 'Bruce Banner']
+names = ['Mr. Fantastic', 'Spider-Man', 'The Hulk', 'Captain America', 'Dr. Strange', 'Hawkeye', 'The Winter Soldier', 'The Pheonix', 'Wolverine (first name only!)', 'Professor X']
+answers = ['Reed Richards', 'Peter Parker', 'Bruce Banner', 'Steve Rodgers', 'Steven Strange', 'Clint Barton', 'Bucky Barnes', 'Jean Gray', 'Logan', 'Charles Xavier']
 
 
 def main():
 
-    user_input = input('Press (s) to start!: ')
-    
-    
-    #question = input(f"What is " + keys['Reed Richards'] + "'s real name?")
-    
+    print()
+    user_input = input('Press (s) to start or anything else to quit!: ')
+    print('--------------------------------------------------')
 
+    #Questions 1-10
     if user_input == 's':
-
+        print()
+        print()
+        print()
         question_1 = input("Question 1: What is " + names[0] + "'s real name? ")
+        print()
         if question_1 == answers[0].lower():
-            print('Correct')
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
-
+        print()
         question_2 = input("Question 2: What is " + names[1] + "'s real name? ")
+        print()
         if question_2 == answers[1].lower():
-            print('Correct')
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
         
-
-        question_3 = input("Question 3: What is " + names[0] + "'s real name? ")
-        if question_1 == answers[0].lower():
-            print('Correct')
+        print()
+        question_3 = input("Question 3: What is " + names[2] + "'s real name? ")
+        print()
+        if question_1 == answers[2].lower():
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
-
-        question_4 = input("Question 4: What is " + names[0] + "'s real name? ")
-        if question_1 == answers[0].lower():
-            print('Correct')
+        print()
+        question_4 = input("Question 4: What is " + names[3] + "'s real name? ")
+        print()
+        if question_1 == answers[3].lower():
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
-
-        question_5 = input("Question 5: What is " + names[0] + "'s real name? ")
-        if question_1 == answers[0].lower():
-            print('Correct')
+        print()
+        question_5 = input("Question 5: What is " + names[4] + "'s real name? ")
+        if question_1 == answers[4].lower():
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
-
-        question_6 = input("Question 6: What is " + names[0] + "'s real name? ")
-        if question_1 == answers[0].lower():
-            print('Correct')
+        print()
+        question_6 = input("Question 6: What is " + names[5] + "'s real name? ")
+        if question_1 == answers[5].lower():
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
-
-        question_7 = input("Question 7: What is " + names[0] + "'s real name? ")
-        if question_1 == answers[0].lower():
-            print('Correct')
+        print()
+        question_7 = input("Question 7: What is " + names[6] + "'s real name? ")
+        if question_1 == answers[6].lower():
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
-
-        question_8 = input("Question 8: What is " + names[0] + "'s real name? ")
-        if question_1 == answers[0].lower():
-            print('Correct')
+        print()
+        question_8 = input("Question 8: What is " + names[7] + "'s real name? ")
+        if question_1 == answers[7].lower():
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
-        
-        question_9 = input("Question 9: What is " + names[0] + "'s real name? ")
-        if question_1 == answers[0].lower():
-            print('Correct')
+        print()
+        question_9 = input("Question 9: What is " + names[8] + "'s real name? ")
+        if question_1 == answers[8].lower():
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
-
-        question_10 = input("Question 10: What is " + names[0] + "'s real name? ")
-        if question_1 == answers[0].lower():
-            print('Correct')
+        print()
+        question_10 = input("Question 10: What is " + names[9] + "'s real name? ")
+        if question_1 == answers[9].lower():
+            print('--------')
+            print('Correct!')
+            print('--------')
         else:
+            print('--------')
             print('Incorrect!')
+            print('--------')
 
         
 
     else:
+        print("Goodbye!")
+        print()
         pass
     
 
